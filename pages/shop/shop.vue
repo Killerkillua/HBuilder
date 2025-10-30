b361fc3b4955bcbf24b9528da71f0f604ae23e40
kk <cxkisskiss@gmail.com>
2025-10-30 15:25:17 +0800

    Add files via upload

---
+++ pages/shop/shop.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="out">
+		<view class="item" v-for="(item,index) in goods" :key="item.id">
+			<checkbox></checkbox>
+			<text class = "title">{{item.name}}</text>
+			<text class = "del" @click="remove(index)">删除</text>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const goods = ref([
+	{id:1, name:"小米"},
+	{id:2, name:"华为"},
+	{id:3, name:"vivo"},
+	{id:4, name:"oppo"},
+])
+
+function remove (index){
+	goods.value.splice(index,1);
+}
+</script>
+
+<style lang="scss" scoped>
+.out{
+	padding:10px;
+	.item{
+		padding: 10px 0;
+		.del{
+			color:#c00;
+			margin-left:10px;
+			}
+		}
+}	
+
+</style>
